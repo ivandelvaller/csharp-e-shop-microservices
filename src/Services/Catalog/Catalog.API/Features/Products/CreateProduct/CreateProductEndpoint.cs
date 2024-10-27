3bf019e0cc2a7775c46e4061e3fc8896bd533698
ivandelvalle <tec.ivandelvaller@outlook.com>
2024-10-27 14:31:43 -0600

    Added Crter methods.

--- src/Services/Catalog/Catalog.API/Features/Products/CreateProduct/CreateProductEndpoint.cs
+++ src/Services/Catalog/Catalog.API/Features/Products/CreateProduct/CreateProductEndpoint.cs
@@ -1,6 +1,36 @@
+using Carter;
+using MediatR;
+using Mapster;
+
 namespace Catalog.API.Features.Products.CreateProduct;
 
-public class CreateProductEndpoint
+public record CreateProductRequest(
+    Guid ProductId,
+    string Name,
+    string Description,
+    string ImageUrl,
+    decimal Price);
+
+public record CreateProductResponse(Guid Id);
+
+public class CreateProductEndpoint : ICarterModule
 {
-    
+    public void AddRoutes(IEndpointRouteBuilder app)
+    {
+        app.MapPost("/products", async (CreateProductRequest request, ISender sender) =>
+            {
+                var command = request.Adapt<CreateProductCommand>();
+
+                var result = await sender.Send(command);
+
+                var response = request.Adapt<CreateProductResponse>();
+
+                return Results.Created($"/products/{result.Id}", response);
+            })
+            .WithName("Create Product")
+            .Produces<CreateProductResult>(StatusCodes.Status201Created)
+            .ProducesProblem(StatusCodes.Status400BadRequest)
+            .WithSummary("Create a Product")
+            .WithDescription("Create a Product");
+    }
 }
--- src/Services/Catalog/Catalog.API/Program.cs
+++ src/Services/Catalog/Catalog.API/Program.cs
@@ -1,9 +1,13 @@
+using Carter;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+builder.Services.AddCarter();
 
 var app = builder.Build();
 
 // Configure the HTTP Request Handler.
+app.MapCarter();
 
 app.Run();
