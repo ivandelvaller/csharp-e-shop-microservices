f7e7d1adf002299fd00298f4cedaff997fd8f645
ivandelvalle <tec.ivandelvaller@outlook.com>
2024-10-27 14:52:53 -0600

    Installed carter on Catalog API.

--- src/Services/Catalog/Catalog.API/Program.cs
+++ src/Services/Catalog/Catalog.API/Program.cs
@@ -4,6 +4,7 @@ var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddCarter();
+builder.Services.AddMediatR(config => { config.RegisterServicesFromAssembly(typeof(Program).Assembly); });
 
 var app = builder.Build();
 
