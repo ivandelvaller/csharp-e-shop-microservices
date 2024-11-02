e9aea73e495fd5ed1fb77db1c49b700083f0c573
ivandelvalle <tec.ivandelvaller@outlook.com>
2024-11-01 18:06:53 -0600

    Fixed property issue.

--- src/Services/Catalog/Catalog.API/Models/Product.cs
+++ src/Services/Catalog/Catalog.API/Models/Product.cs
@@ -2,7 +2,7 @@ namespace Catalog.API.Models;
 
 public class Product : BaseModel
 {
-    public Guid Id { get; set; };
+    public Guid Id { get; set; }
 
     public string Name { get; set; } = default!;
 
